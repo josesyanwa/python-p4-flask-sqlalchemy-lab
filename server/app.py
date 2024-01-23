52cf8ab9c2314e0c53cd9b63d0578fd86426d318
Joses Syanwa <josesyanwa@gmail.com>
2024-01-23 13:56:04 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask, render_template
 from flask_migrate import Migrate
 
 from models import db, Zookeeper, Enclosure, Animal
@@ -19,16 +17,18 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get_or_404(id)
+    return render_template('animal.html', animal=animal)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get_or_404(id)
+    return render_template('zookeeper.html', zookeeper=zookeeper)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get_or_404(id)
+    return render_template('enclosure.html', enclosure=enclosure)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
